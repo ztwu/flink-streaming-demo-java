5fc5021e546dc04a22631d9e6d0411d00b8a1fea
ztwu4 <ztwu4@iflytek.com>
2021-04-23 17:49:42 +0800

    yh

--- src/main/java/com/ztwu/bigdata/demo/aggregation/AggregationFunctionDemo.java
+++ src/main/java/com/ztwu/bigdata/demo/aggregation/AggregationFunctionDemo.java
@@ -2,7 +2,9 @@ package com.ztwu.bigdata.demo.aggregation;
 
 import org.apache.flink.api.common.functions.AggregateFunction;
 import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.java.aggregation.Aggregations;
 import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
@@ -12,7 +14,6 @@ import org.apache.flink.streaming.api.windowing.assigners.SlidingProcessingTimeW
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
-import scala.Tuple2;
 
 public class AggregationFunctionDemo {
 	public static void main(String[] args) throws Exception {
@@ -49,42 +50,45 @@ public class AggregationFunctionDemo {
 		});
 
 		inputStream
-				.keyBy((KeySelector<Tuple2<Long, String>, String>) value -> value._2)
+				.keyBy((KeySelector<Tuple2<Long, String>, String>) value -> value.f1)
 //				.keyBy(new KeySelector<Tuple2<Long, String>, String>() {
 //			@Override
 //			public String getKey(Tuple2<Long, String> rowData) throws Exception {
+//				scala语法 tuple2 ==> ._2
 //				return rowData._2;
 //			}
 //		})
 //				.window(SlidingProcessingTimeWindows.of(Time.seconds(6), Time.seconds(2)))
 				.window(TumblingProcessingTimeWindows.of(Time.seconds(5)))
 //				.timeWindow(Time.seconds(2))
-				.aggregate(new AggregateFunction<Tuple2<Long, String>, Long, Long>() {
-
-					@Override
-					public Long createAccumulator() {
-						/*访问量初始化为0*/
-						return 0L;
-					}
-
-					@Override
-					public Long add(Tuple2<Long, String> value, Long accumulator) {
-						/*访问量直接+1 即可*/
-						return accumulator+1;
-					}
-
-					@Override
-					public Long getResult(Long accumulator) {
-						return accumulator;
-					}
-
-					/*合并两个统计量*/
-					@Override
-					public Long merge(Long a, Long b) {
-						return a+b;
-					}
-
-				}).print();
+				.sum(0)
+				.print();
+//				.aggregate(new AggregateFunction<Tuple2<Long, String>, Long, Long>() {
+//
+//					@Override
+//					public Long createAccumulator() {
+//						/*访问量初始化为0*/
+//						return 0L;
+//					}
+//
+//					@Override
+//					public Long add(Tuple2<Long, String> value, Long accumulator) {
+//						/*访问量直接+1 即可*/
+//						return accumulator+1;
+//					}
+//
+//					@Override
+//					public Long getResult(Long accumulator) {
+//						return accumulator;
+//					}
+//
+//					/*合并两个统计量*/
+//					@Override
+//					public Long merge(Long a, Long b) {
+//						return a+b;
+//					}
+//
+//				}).print();
 
 		env.execute("test");
 
